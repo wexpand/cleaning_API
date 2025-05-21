f6ef4fb4ae6e874c0970a41df7468ebe62d44e23
wexpand <marketing@wexpandmx.com>
2025-05-21 15:25:28 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host='35.208.37.65',
-        dbname='dbmupvjj30fiep',
-        user='u3jxzcgl1vira',
-        password='gj3nxiotwqvm',
-        port=5432
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT", "5432"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
